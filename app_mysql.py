4280aece6499c9838e8460c32126b71eae32b30b
ybotia555 <143663933+ybotia555@users.noreply.github.com>
2026-04-21 15:39:11 -0500

    Update app_mysql.py

--- app_mysql.py
+++ app_mysql.py
@@ -44,10 +44,10 @@ app.secret_key = "gestorpro_v13_ultra_secure_2025"
 # ================================================================
 import os
 
-DB_HOST     = os.getenv("DB_HOST")
-DB_USER     = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME     = os.getenv("DB_NAME")
+DB_HOST     = os.getenv("DB_HOST", "mainline.proxy.rlwy.net")
+DB_USER     = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_NAME     = os.getenv("DB_NAME", "railway")
 DB_PORT     = int(os.getenv("DB_PORT", 3306))
 
 # ================================================================
